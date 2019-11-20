85a09fb7045d4d93bd3308c34e19a1a4bf9d6425
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-11-20 15:09:24 -0600

    exercise complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("So Far So Good!");
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+        String myString = "Still doing good";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+        int r = 4 + 5;
+        int q = 3;
+        int c = 4 * q;
+        int b = 10;
+        int a = 2;
+        b /= a;
+        a -= b;
+        System.out.println(r);
+        System.out.println(q);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
