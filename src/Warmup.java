337b461c3496029bd7f36f98425d6673ddb272bd
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-01-08 11:31:33 -0600

    added FizzBuzz to Java warmup

---
+++ src/Warmup.java
@@ -0,0 +1,49 @@
+    import java.util.*;
+public class Warmup {
+
+//    public static void main(String[] args)
+//    {
+//
+//        char[] chars = {'a','k','f','z'};
+//        for (char letter : chars) {
+//            char next = letter;
+//            switch (next) {
+//                case 'z': // or 'z', either way
+//                    next ='a';
+//                    break;
+//                default:
+//                    next += 1;
+//            }
+//            System.out.println(letter + " is followed by " + next + " ");
+//        }
+//
+//    }
+
+
+    public static void main(String args[]) {
+        int n = 60;
+
+        // loop for 60 times
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0)
+                System.out.print("FizzBuzz" + " ");
+                // number divisible by 5, print 'Buzz'
+                // in place of the number
+            else if (i % 5 == 0)
+                System.out.print("Buzz" + " ");
+
+                // number divisible by 3, print 'Fizz'
+                // in place of the number
+            else if (i % 3 == 0)
+                System.out.print("Fizz" + " ");
+
+                // number divisible by 15(divisible by
+                // both 3 & 5), print 'FizzBuzz' in
+                // place of the number
+
+            else // print the numbers
+                System.out.print(i + " ");
+        }
+    }
+}
+
