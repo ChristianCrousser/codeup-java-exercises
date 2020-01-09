dbccb48879905719ce8b8e25da44599b88f242e4
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-01-09 09:26:34 -0600

    warmup

--- src/Warmup.java
+++ src/Warmup.java
@@ -20,30 +20,46 @@ public class Warmup {
 //    }
 
 
-    public static void main(String args[]) {
-        int n = 60;
+//    public static void main(String args[]) {
+//        int n = 60;
+//
+//        // loop for 60 times
+//        for (int i = 1; i <= n; i++) {
+//            if (i % 15 == 0)
+//                System.out.print("FizzBuzz" + " ");
+//                // number divisible by 5, print 'Buzz'
+//                // in place of the number
+//            else if (i % 5 == 0)
+//                System.out.print("Buzz" + " ");
+//
+//                // number divisible by 3, print 'Fizz'
+//                // in place of the number
+//            else if (i % 3 == 0)
+//                System.out.print("Fizz" + " ");
+//
+//                // number divisible by 15(divisible by
+//                // both 3 & 5), print 'FizzBuzz' in
+//                // place of the number
+//
+//            else // print the numbers
+//                System.out.print(i + " ");
+//        }
+//    }
 
-        // loop for 60 times
-        for (int i = 1; i <= n; i++) {
-            if (i % 15 == 0)
-                System.out.print("FizzBuzz" + " ");
-                // number divisible by 5, print 'Buzz'
-                // in place of the number
-            else if (i % 5 == 0)
-                System.out.print("Buzz" + " ");
+//    public static int SimpleAdding(int num) {
+//        int bucket = 0;
+//        for (int i = 1; i <= num; i++) {
+//            bucket += i;
+//        }
+//
+//        return bucket;
+//    }
+//
+//    public static void main (String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        System.out.println(SimpleAdding(s.nextLine()));
+//    }
 
-                // number divisible by 3, print 'Fizz'
-                // in place of the number
-            else if (i % 3 == 0)
-                System.out.print("Fizz" + " ");
-
-                // number divisible by 15(divisible by
-                // both 3 & 5), print 'FizzBuzz' in
-                // place of the number
-
-            else // print the numbers
-                System.out.print(i + " ");
-        }
-    }
 }
+
 
