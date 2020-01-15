83aef8a5607cbfc059c142c49c072e400e5c38f2
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-01-15 09:19:08 -0600

    warmup

--- src/Warmup.java
+++ src/Warmup.java
@@ -88,6 +88,17 @@ public class Warmup {
 //    }
 
 
+//    public class EvensOnly {
+//        public static void main(String[] args){
+//            for (int i = 1; i < 100; i++) {
+//                if (i % 2 == 0) {
+//                    System.out.println(i);
+//                }
+//            }
+//        }
+//    }
+
+
 }
 
 
